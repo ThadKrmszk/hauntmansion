24416967386ab75b2a6ec0ace6027e2ac230002b
ThadKrmszk <Teerasak.thad@bumail.net>
2021-11-28 12:55:17 +0700

    testv1

---
+++ Test game/Assets/code/playerScript/MapOpen.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapOpen : MonoBehaviour
+{
+    [SerializeField] public GameObject map;
+    private bool isMap;
+    public static bool haveAMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        haveAMap = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (haveAMap == false)
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                Debug.Log("i dont have a map");
+            }
+        }
+        if (haveAMap)
+        {
+             if (Input.GetKeyDown(KeyCode.M))
+                   {
+                       isMap = !isMap;
+                       
+                       if (isMap)
+                        
+                           map.SetActive(true); 
+                        
+                       else
+                        
+                           map.SetActive(false); 
+                        
+                   } 
+        }
+        //Open map
+      
+    }
+
+   
+}
